9c83a3cbdac6c1332d0747bff1e1a3b08a4d3a0f
kassy-ux <stancyngereso4@gmail.com>
2025-07-25 17:05:53 +0300

    dist

--- dist/server.js
+++ dist/server.js
@@ -26,12 +26,10 @@ app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(logger_1.logger);
-// app.use(rateLimiterMiddleware);
 //default route
 app.get('/', (req, res) => {
     res.send("Welcome to Express API Backend WIth Drizzle ORM and PostgreSQL");
 });
-// Enable CORS for all routes
 // Or configure specific origins
 app.use((0, cors_1.default)({
     origin: 'http://localhost:5173',
